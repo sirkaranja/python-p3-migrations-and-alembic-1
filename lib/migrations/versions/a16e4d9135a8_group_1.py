f47b8d8d6b475d55193e01762fcd9ee2777352df
+ <+>
2023-06-04 13:06:49 +0300

    migrations

---
+++ lib/migrations/versions/a16e4d9135a8_group_1.py
@@ -0,0 +1,24 @@
+"""Group 1
+
+Revision ID: a16e4d9135a8
+Revises: 
+Create Date: 2023-05-31 14:33:47.804839
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a16e4d9135a8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
